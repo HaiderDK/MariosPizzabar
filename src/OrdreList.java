ae505ec1410ab60cf36877453d1a1d8a0ee8c0cb
erkam22 <erkamo3032@gmail.com>
2025-03-25 13:34:25 +0100

    hey

--- src/OrdreList.java
+++ src/OrdreList.java
@@ -1,7 +1,43 @@
-import java.util.ArrayList;
-import java.util.List;
 
-public class OrdreList {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.time.LocalDateTime;
+
+    public class OrdreList {
+
+        private ArrayList<Ordre> activeOrders;
+
+        public OrdreList() {
+            this.activeOrders = new ArrayList<>();
+        }
+
+        // Tilføjer en ordre og opdaterer listen efter automatisk
+        public void addOrdre(Ordre ordre) {
+            activeOrders.add(ordre);
+            sortOrdrerByTime();
+            System.out.println("Ny ordre tilføjet: " + ordre);
+        }
+
+        // Fjerner en ordre
+        public void removeOrdre(int ordreId) {
+            activeOrders.removeIf(ordre -> ordre.getOrdreId() == ordreId);
+            System.out.println("Ordre #" + ordreId + " er fjernet.");
+        }
+
+        // ordre sorteres efter bestillingstid
+        public void sortOrdrerByTime() {
+            Collections.sort(activeOrders, Comparator.comparing(Ordre::getOrdreTime));
+        }
+
+        // her vises alle de aktive order der er
+        public void displayOrdrer() {
+            System.out.println("\nAktive Ordrer:");
+            for (Ordre ordre : activeOrders) {
+                System.out.println(ordre);
+            }
+        }
+    }
 
 }
 
